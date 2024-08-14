37f677ef3d2b3700489f30feba02b837302a84ce
Jibrand <muhammadjibrandev@gmail.com>
2024-08-14 01:03:45 -0700

    updated image

--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -1,7 +1,3 @@
-import image1 from "@/public/images/testimonial/gopal.jpg";
-import image2 from "@/public/images/testimonial/adriano.jpg";
-import image3 from "@/public/images/testimonial/azizurrehman.jpg";
-import image4 from "@/public/images/testimonial/Grace Kim.png";
 import { Testimonial } from "@/types/testimonial";
 
 export const testimonialData: Testimonial[] = [
@@ -9,7 +5,7 @@ export const testimonialData: Testimonial[] = [
     id: 1,
     name: "Gopal Dutt",
     designation: "Founder at Alchemist",
-    image: image1,
+    image: '/images/testimonial/gopal.jpg',
     content:
       "Working with this was an absolute pleasure! They not only delivered an exceptional website in record time but also demonstrated excellent communication skills throughout the process. Their top-notch skills in modern web development truly shined. Highly recommended!",
   },
@@ -17,7 +13,7 @@ export const testimonialData: Testimonial[] = [
     id: 2,
     name: "Adriano Meza",
     designation: "CEO at BEEQUEENCOIN",
-    image: image2,
+    image: '/images/testimonial/adriano.jpg',
     content:
       "Fast, efficient, and incredibly talented! This exceeded all my expectations with swift delivery and top-notch web development skills. Impeccable communication made the experience smooth and enjoyable. Looking forward to the next project!",
   },
@@ -25,7 +21,7 @@ export const testimonialData: Testimonial[] = [
     id: 3,
     name: "Aziz-ur Rehman",
     designation: "Head of Digital D and I, Apps4free Ltd",
-    image: image3,
+    image: '/images/testimonial/azizurrehman.jpg',
     content:
       "Look no further for exceptional results at lightning speed! This top niche skills in web development are second to none, and their clear communication sets them apart. Impressed by their effortless ability to turn ideas into reality. A true gem in web design!",
   },
@@ -33,7 +29,7 @@ export const testimonialData: Testimonial[] = [
     id: 4,
     name: "Garce Kim",
     designation: "CEO of TheIKAgency",
-    image: image4,
+    image: '/images/testimonial/Grace Kim.png',
     content:
       "This transformed our online presence with their incredible skills and attention to detail. Their approach was both innovative and practical, delivering results that exceeded our expectations. Communication was smooth and professional throughout. Highly recommend!",
   },
