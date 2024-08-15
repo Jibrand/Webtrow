80ce3ce9cf82c27f0a108ff13667d4deba13acdd
Jibrand <muhammadjibrandev@gmail.com>
2024-08-15 09:48:09 -0700

    main: Added GTM

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Webtrow - Boost your sales, enhance your brand, and drive more appointments with our expert website design services. Double your income today with a free consultation." />
       <link rel="icon" href="/images/favcon.ico" />
+      
     </>
   );
 }
